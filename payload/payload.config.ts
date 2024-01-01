d14a0a11f9a5fd642ead081da19b64d60e03083c
Dmitrij Kosow <dymitrkosow@gmail.com>
2024-01-01 20:20:06 +0100

    settings server part1

---
+++ payload/payload.config.ts
@@ -0,0 +1,40 @@
+import { buildConfig } from 'payload/config'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import path from 'path'
+import dotenv from 'dotenv'
+
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+})
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
+  collections: [
+    // Users, Products, Media, ProductFiles, Orders
+], 
+  routes: {
+    admin: '/sell',
+  },
+  admin: {
+    user: 'users',
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: '- DigitalHippo',
+      favicon: '/favicon.ico',
+      ogImage: '/thumbnail.jpg',
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+})
